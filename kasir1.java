4e7e8156c6d4537ce81870e663317049576bff94
Eko Aryanto <ekoaryanto11@gmail.com>
2024-03-25 20:46:03 +0700

    add comment

--- kasir1.java
+++ kasir1.java
@@ -1,14 +1,15 @@
 public class kasir1 {
     public static void main(String[] args) {
-        String nm_toko = "TOKO MENU A";
-        String alamat1 = "Jalan In Aja Dulu";
-        String alamat2 = "Ke Pelamin A, Jakarta Tengah";
+        // initialise int variable
+        String nm_toko = "TOKO SUMBER REJEKI";
+        String alamat1 = "Jl. Raya Kampung Tengah No 80 Kel. Gedong";
+        String alamat2 = "Kec. Pasar Rebo, Jakarta Timur 13760";
 
-        String nm_brg1 = "Luwak Black Coffe";
+        String nm_brg1 = "Luwak White Coffe";
         int qty1 = 10;
         int hrg1 = 9200;
 
-        String nm_brg2 = "Pop Mie Likin Mu";
+        String nm_brg2 = "Pop Mie Baso 75g";
         int qty2 = 5;
         int hrg2 = 4000;
 
@@ -20,6 +21,7 @@ public class kasir1 {
         int total_qty = qty1 + qty2;
         double total = subtotal - ppn;
 
+        // output
         System.out.println(nm_toko);
         System.out.println(alamat1);
         System.out.println(alamat2);
@@ -32,8 +34,8 @@ public class kasir1 {
         System.out.println("Subtotal = " + subtotal);
         System.out.println("Ppn (10%) = " + Math.round(ppn));
         System.out.println("----------------------------------------------------------------");
-        System.out.println("Subtotal = " + total_qty);
-        System.out.println("Ppn (10%) = " + Math.round(total));
+        System.out.println("Total Qty = " + total_qty);
+        System.out.println("Total = " + Math.round(total));
         System.out.println("================================================================");
         System.out.println();
         System.out.println("Barang yang sudah dibeli");
